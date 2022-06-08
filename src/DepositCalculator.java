4bc80d364477f54cceb0e7d49d51ffdf2b6f3edf
Eduard <minnibaev-ef@yandex.ru>
2022-06-08 23:49:52 +0300

    style: не исправил два метода

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class DepositCalculator {
     double calculateComplexPercent(double a, double y, int d) {
         double pay = a * Math.pow((1 + y / 12), 12 * d);
-        return random(pay, 2);
+        return round(pay, 2);
     }
 
     double calculateSimplePercent(double doubleAmount, double double_year_rate, int deposit_period) {
-        return random(doubleAmount + doubleAmount * double_year_rate * deposit_period, 2);
+        return round(doubleAmount + doubleAmount * double_year_rate * deposit_period, 2);
     }
 
-    double random(double value, int places) {
+    double round(double value, int places) {
         double scale = Math.pow(10, places);
         return Math.round(value * scale) / scale;
     }
 
-    void doImportantJob() {
+    void getDeposit() {
         int period;
-        int action;
+        int command;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:");
@@ -25,16 +25,16 @@ public class DepositCalculator {
         System.out.println("Введите срок вклада в годах:");
         period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
-        action = scanner.nextInt();
+        command = scanner.nextInt();
         double totalDeposit = 0;
-        if (action == 1) {
+        if (command == 1) {
             totalDeposit = calculateSimplePercent(amount, 0.06, period);
-        } else if (action == 2) {
+        } else if (command == 2) {
             totalDeposit = calculateComplexPercent(amount, 0.06, period);
         }
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + totalDeposit);
     }
     public static void main(String[] args) {
-        new DepositCalculator().doImportantJob();
+        new DepositCalculator().getDeposit();
     }
 }
